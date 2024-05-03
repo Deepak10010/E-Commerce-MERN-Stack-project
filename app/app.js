ad134a273f9b3c3a5d409f34cd660b1c07f4e03e
Deepak10010 <deepakdoddera@gmail.com>
2024-05-03 22:01:45 +0200

    User Registration controller

--- app/app.js
+++ app/app.js
@@ -2,14 +2,17 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import dbConnect from "../config/dbConnect.js";
-
+import userRoutes from "../routes/usersRoute.js";
 
 dotenv.config();
 
 
 //db connect
 dbConnect();
-
 const app = express();
+
+
+//routes
+app.use('/',userRoutes)
 
 export default app;
