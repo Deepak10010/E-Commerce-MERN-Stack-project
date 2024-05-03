d365f65a6b0ebb2737af133611e5b2c4daa76ba8
Deepak10010 <deepakdoddera@gmail.com>
2024-05-03 22:36:51 +0200

    Hash User password

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -1,5 +1,5 @@
 import User from "../model/User.js";
-
+import bcrypt from "bcryptjs";
 
 // @desc    Register user
 // @route   POST /api/v1/users/register
@@ -17,9 +17,19 @@ export const registerUserCtrl = async (req, res) => {
     }
 
     //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+
+
+
+
     //create the user
-    const user = await User.create({fullname, email, password});
-    res.status(201).json({
+    const user = await User.create({
+        fullname, 
+        email, 
+        password: hashedPassword
+        });
+        res.status(201).json({
         status : 'success',
         message : 'User registered successfully',
         data : user,
